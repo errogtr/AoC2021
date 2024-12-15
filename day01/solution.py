543409aaaff022235de0d6b2805d56cb015663c8
enrico <enrico.dsf@gmail.com>
2024-12-15 01:54:51 +0100

    project template

---
+++ day01/solution.py
@@ -0,0 +1,12 @@
+def sweep(idx):
+    return sum(y > x for x, y in zip(data, data[idx:]))
+
+
+with open("data") as f:
+    data = [int(line) for line in f.read().splitlines()]
+
+# ---- PART 1 ----
+print(sweep(1))
+
+# ---- PART 2 ----
+print(sweep(3))
